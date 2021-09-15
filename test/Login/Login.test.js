e2d14c8bf07f1b636de336e76bd2ccc3e9388a35
Maikel Maas <mmaas@sphereon.com>
2021-09-15 13:06:50 +0200

    ONTO-51 Setup React Native skeleton library for Auth
       - Created a basic react-native component library structure;
       - Added UI test example;

---
+++ test/Login/Login.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, fireEvent } from '@testing-library/react-native';
+
+import {Login} from "../../src/Login";
+
+test('Fort submit credentials', () => {
+    const mockFn = jest.fn();
+
+    const { getByPlaceholderText, getByA11yRole} = render(
+        <Login onSubmit={mockFn} />
+    );
+
+    const username = getByPlaceholderText('Username');
+    const password = getByPlaceholderText('Password');
+    const button = getByA11yRole('button')
+
+    fireEvent.changeText(username, 'Test');
+    fireEvent.changeText(password, 'test123');
+    fireEvent.press(button);
+
+    expect(mockFn).toBeCalledWith({ username: 'Test', password: 'test123' });
+});
