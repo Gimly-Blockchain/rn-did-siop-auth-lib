3c7300f8e08a3f048bf9749394c53ab8bb517c66
Sander <spostma@sphereon.com>
2021-09-27 17:07:52 +0200

    Split up calls

--- src/OPAuthenticator.ts
+++ src/OPAuthenticator.ts
@@ -1,80 +1,87 @@
 import {OP} from "@sphereon/did-auth-siop/dist/main";
+import {VerifiedAuthenticationRequestWithJWT} from "@sphereon/did-auth-siop/dist/main/types/SIOP.types";
+
 import {
-  AuthenticationResponseOpts,
-  PassBy,
-  ResponseMode,
-  VerificationMode,
-  VerifyAuthenticationRequestOpts
+    AuthenticationResponseOpts,
+    PassBy,
+    ResponseMode,
+    VerificationMode,
+    VerifyAuthenticationRequestOpts
 } from "@sphereon/did-auth-siop/dist/main/types/SIOP.types";
 import axios from "axios"
 import './shim'
 
-// TODO Add configuration method for the DID & private key
-const HEX_KEY = "c848751f600a9b8b91e3db840d75be2304b0ec4b9b15fe77d87d3eed9a007d1a";
-const DID = "did:ethr:0x8D0E24509b79AfaB3A74Be1700ebF9769796B489";
+//const HEX_KEY = "c848751f600a9b8b91e3db840d75be2304b0ec4b9b15fe77d87d3eed9a007d1a";
+//const DID = "did:ethr:0x8D0E24509b79AfaB3A74Be1700ebF9769796B489";
 
 export default class OPAuthenticator {
 
-  // noinspection JSUnusedGlobalSymbols
-  public async executeLoginFlowFromQR(redirectUrl: string, state: string): Promise<void> {
-    const getRequestUrl = redirectUrl + "?stateId=" + state;
-    console.log("getRequestUrl", getRequestUrl);
-    const response = await axios.get(getRequestUrl)
-    console.log("response.status", response.status);
-    if (response.status == 200) {
-      const uriDecoded = decodeURIComponent(response.data as string);
-      const requestURI = OPAuthenticator.objectFromURI(uriDecoded)
-      await this.verifyAuthenticationRequestURI(requestURI)
-      return
-    } else {
-      throw new Error("Could not fetch the request URL: " + response.statusText || response.data)
+    private opDID: string;
+    private opPrivateKey: string;
+
+
+    constructor(opDID: string, opPrivateKey: string) {
+        this.opDID = opDID
+        this.opPrivateKey = opPrivateKey
     }
-  }
 
-  private async verifyAuthenticationRequestURI(requestURI: any): Promise<void> {
-    const responseOpts: AuthenticationResponseOpts = {
-      signatureType: {
-        hexPrivateKey: HEX_KEY,
-        did: DID
-      },
-      registration: {
-        registrationBy: {
-          type: PassBy.VALUE,
-        },
-      },
-      responseMode: ResponseMode.POST,
-      did: DID,
-      expiresIn: 2000
-    };
-
-    const verifyOpts: VerifyAuthenticationRequestOpts = {
-      verification: {
-        mode: VerificationMode.INTERNAL,
-        resolveOpts: {
-          didMethods: ["ethr"]
+// noinspection JSUnusedGlobalSymbols
+    public async getRequestUrl(redirectUrl: string, state: string): Promise<string> {
+        const getRequestUrl = redirectUrl + "?stateId=" + state;
+        console.log("getRequestUrl", getRequestUrl);
+        const response = await axios.get(getRequestUrl)
+        console.log("response.status", response.status);
+        if (response.status == 200) {
+            const uriDecoded = decodeURIComponent(response.data as string);
+            return OPAuthenticator.objectFromURI(uriDecoded)
+        } else {
+            throw new Error("Could not fetch the request URL: " + response.statusText || response.data)
         }
-      },
-      nonce: requestURI.nonce
     }
 
-    const op = OP.fromOpts(responseOpts, verifyOpts);
-    const jwt = requestURI.request;
-    await op.verifyAuthenticationRequest(jwt, {});
-    await this.sendAuthResponse(op, jwt, requestURI);
-    return
-  }
+    public async verifyAuthenticationRequestURI(requestURI: any): Promise<VerifiedAuthenticationRequestWithJWT> {
+        const responseOpts: AuthenticationResponseOpts = {
+            signatureType: {
+                hexPrivateKey: this.opPrivateKey,
+                did: this.opDID
+            },
+            registration: {
+                registrationBy: {
+                    type: PassBy.VALUE,
+                },
+            },
+            responseMode: ResponseMode.POST,
+            did: this.opDID,
+            expiresIn: 2000
+        };
 
-  private async sendAuthResponse(op: OP, requestJwt: string, requestURI: any): Promise<void> {
-    const authResponse = await op.createAuthenticationResponse(requestJwt)
-    const siopSessionResponse = await axios.post(requestURI.redirect_uri, authResponse)
-    if (siopSessionResponse.status == 200) {
-      return
-    } else {
-      throw new Error(`Error ${siopSessionResponse.status}: ${siopSessionResponse.statusText}`)
+        const verifyOpts: VerifyAuthenticationRequestOpts = {
+            verification: {
+                mode: VerificationMode.INTERNAL,
+                resolveOpts: {
+                    didMethods: ["ethr"]
+                }
+            },
+            nonce: requestURI.nonce
+        }
+
+        const op = OP.fromOpts(responseOpts, verifyOpts);
+        const jwt = requestURI.request;
+        return await op.verifyAuthenticationRequest(jwt, {});
     }
-  }
 
-  private static objectFromURI(uriDecoded: string): string {
-    return JSON.parse('{"' + uriDecoded.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
-  }
+
+    public async sendAuthResponse(op: OP, requestJwt: string, redirectURI: string): Promise<void> {
+        const authResponse = await op.createAuthenticationResponse(requestJwt)
+        const siopSessionResponse = await axios.post(redirectURI, authResponse)
+        if (siopSessionResponse.status == 200) {
+            return
+        } else {
+            throw new Error(`Error ${siopSessionResponse.status}: ${siopSessionResponse.statusText}`)
+        }
+    }
+
+    private static objectFromURI(uriDecoded: string): string {
+        return JSON.parse('{"' + uriDecoded.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import OPAuthenticator from "./OPAuthenticator";
 
-export type { QRType, QRMode, QRContent } from 'gimlyid-qr-code/dist';
 export default OPAuthenticator
